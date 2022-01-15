55f0b45774b08f1fe85102ee368993e84a692daf
HackBugZ <94540527+HackBugZ@users.noreply.github.com>
2022-01-15 16:00:22 +0100

    Create head.php

---
+++ head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML>
+<html id="html">
+<head id="head">
+<title>#TITLE#</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=4.0;" />
+<meta name="expires" content="0">
+</head>
